60581cc473338bc2812ccedf1f936ece0a87cc1c
Usova_le12@top-academy.ru <Usova_le12@top-academy.ru>
2025-09-27 21:34:08 +0300

    Add project files.

---
+++ ContosoUniversity/Models/Enrollment.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+	public enum Grade { A, B, C, D, E, F }
+	public class Enrollment
+	{
+		public int EnrollmentID { get; set; }
+		public int CoutseID { get; set; }
+		public int StudentID { get; set; }
+		public Grade? Grade { get; set; }
+
+		//Navigation property:
+		public Student Student { get; set; }
+		public Course Course { get; set; }
+	}
+}
